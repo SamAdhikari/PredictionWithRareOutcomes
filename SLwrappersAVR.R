4289aa08a9f82c1433c90c9e0c9a4ada6bcad7af
SamAdhikari <adhikari@hcp.med.harvard.edu>
2018-02-15 16:08:10 -0500

    new predict functions for the wrappers

--- SLwrappersAVR.R
+++ SLwrappersAVR.R
@@ -1,59 +1,100 @@
-### New Super learner wrappers
-### 1. SL.glmnet.T0 : glmnet function to run lasso regression without penalty on the treatments
-### 2. SL.glasso : funtion to run group lasso penalized regression
-### 3. SL.sparseglasso : function to run sparse group lasso penalized regression
-### 4. SL.logistf : SL wrapper to fit logistic regression using Firth's bias reduction method
-
-### other methods included in the function: SL.glm, SL.glmnet, SL.randomforest
-
-### SLwrapperALL : wrapper function to run CV.SuperLearner on externally specified data, group indicators 
-### for group lasso and sparse group lasso and penalty.factor for SL.glmnet.T0
-
-##Input:
-##  X: matrix of predictors
-##  Y: vector of output (currently only handles binary data)
-##  method: method of CV.SuperLearner
-##  groupIndicators: a vector with group identifiers of the predictors to use for SL.glasso and SL.sparseglasso
-##  lambdaseq: sequence of lambda values (in decreasing order) to for SL.sparseglasso
-##  penalty.factor: a vector with identifier for the treatments that are not to be penalized in SL.glmnet.T0  
-
-
-##Output: An object of class CV.SuperLearner
-
-library(SuperLearner)
-SLwrapperALL = function(X,Y,method,groupIndicators,
-                        lambdaseq,penalty.factor,...)
+##I am using this function fromt the package SuperLearner
+##for some reason R in linux is not recognizing this function in the learner
+create.Learner =
+function (base_learner, params = list(), tune = list(), env = parent.frame(), 
+          name_prefix = base_learner, detailed_names = F, verbose = F) 
 {
-    ###glmnet with no penalization for the treatments
-    SL.glmnet.T0 = function(Y=Y,X=X,newX=newX,nfolds=10,
-                            family=family,
-                            penalty.factor=penanlty.factor,
-                            ...)
-        {
-        if (!is.matrix(X)) {
-            X <- model.matrix(~ -1 + ., X)
-            newX <- model.matrix(~ -1 + ., newX)
-        }
-        cv.fit.lasso.up = cv.glmnet(X=X,Y=Y,family=family,
-                                standardize=FALSE,
-                                penalty.factor=penalty.factor, 
-                                nfolds=nfolds)
-        pred = predict(cv.fit.lasso.up,newX,s='lambda.min')
-        fit = list(object = cv.fit.lasso.up)
-        out = list(fit,pred)
-        class(out$fit) = 'SL.glmnet'
-        return(out)
+    if (length(tune) > 0) {
+        tuneGrid = expand.grid(tune, stringsAsFactors = FALSE)
+        names = rep("", nrow(tuneGrid))
+        max_runs = nrow(tuneGrid)
     }
-    
-    #########   group lasso penalization
-    SL.glasso = function(Y, X, newX, 
-                         family=family,
-                         groupid = groupIndicators,
-                         nfolds=10,lambda=100,
-                         loss='logit',...)
+    else {
+        max_runs = 1
+        tuneGrid = NULL
+        names = c()
+    }
+    for (i in seq(max_runs)) {
+        name = paste(name_prefix, i, sep = "_")
+        if (length(tune) > 0) {
+            g = tuneGrid[i, , drop = F]
+            if (detailed_names) {
+                name = do.call(paste, c(list(name_prefix), g, 
+                                        list(sep = "_")))
+            }
+        }
+        else {
+            g = c()
+        }
+        names[i] = name
+        fn_params = ""
+        all_params = c(as.list(g), params)
+        for (name_i in names(all_params)) {
+            val = all_params[[name_i]]
+            if (!is.null(val) && val != "NULL") {
+                if (class(val) == "character") {
+                    val = paste0("\"", val, "\"")
+                }
+                fn_params = paste0(fn_params, ", ", name_i, "=", 
+                                   val)
+            }
+        }
+        fn = paste0(name, " <- function(...) ", base_learner, 
+                    "(...", fn_params, ")")
+        if (verbose) {
+            cat(fn, "\n")
+        }
+        eval(parse(text = fn), envir = env)
+    }
+    results = list(grid = tuneGrid, names = names, base_learner = base_learner, 
+                   params = params)
+    invisible(results)
+}
+
+
+
+##glmnet without penalty on the treatments
+SL.glmnetT0 <- function(Y, X, newX, family, obsWeights, id,
+                                     alpha = 1, nfolds = 10, nlambda = 100, useMin = TRUE,
+                                     loss = "deviance",
+                                     ...) 
     {
+    require('glmnet')
+    if (!is.matrix(X)) {
+        X <- model.matrix(~ -1 + ., X)
+        newX <- model.matrix(~ -1 + ., newX)
+    }
+    p.factor = rep(1,dim(X)[2])
+    p.factor[grep('Valve_G',dimnames(X)[[2]])] = 0 #no penalty on the valves
+    # Use CV to find optimal lambda.
+    fitCV <- glmnet::cv.glmnet(x = X, y = Y, 
+                               type.measure = loss,
+                               nfolds = nfolds,
+                               family = family$family,
+                               nlambda = nlambda,
+                               penalty.factor=p.factor,
+                               ...)
+    # If we predict with the cv.glmnet object we can specify lambda using a
+    # string.
+    pred <- predict(fitCV, newx = newX, type = "response",
+                    s = ifelse(useMin, "lambda.min", "lambda.1se"))
+    fit <- list(object = fitCV, useMin = useMin)
+    class(fit) <- "SL.glmnet"
+    out <- list(pred = pred, fit = fit)
+    return(out)
+}
+
+#########   group lasso penalization
+SL.glasso = function(Y, X, newX, 
+                         family,
+                         groupid=1:dim(X)[2],
+                         nfolds=10,
+                        ...)
+{
+       # groupid = getGroupsFoldsAVR(TT,X,Y)$groupIndicators
+     #   groupid = 1:dim(X)[2]
         require(gglasso) #package to fit group lasso
-        if (family$family == "gaussian") {
+        if(family$family == "gaussian") {
             stop("SL.glasso only available for family = binomial()")
         }
         if(length(groupid)!= dim(X)[2]){
@@ -66,30 +107,43 @@ SLwrapperALL = function(X,Y,method,groupIndicators,
         Y[which(Y==0)] = -1
         fit.CVglasso = gglasso::cv.gglasso(x=X, y=Y, 
                                            group=groupid,
-                                           loss=loss,
+                                           pred.loss='loss',
                                            nfolds=nfolds)
         predict.gglasso <- predict(fit.CVglasso,
                                    newX,
-                                   s=fit.CVglasso$lambda.min,
+                                   s="lambda.min",
                                    type='link')
         pred = sapply(1:length(predict.gglasso),
                       function(x)1/(1+exp(-predict.gglasso[x])))
-        
-        fit <- list(object = fit.CVglasso )
+        fit <- list(object = fit.CVglasso)
         out <- list(pred = pred, fit = fit)
         class(out$fit) <- 'SL.glasso'
         return(out)
-    }
+}
+
     
-    ######### sparse group lasso penalization
-    SL.sparseglasso = function(Y, X, newX, family, 
-                               grouping = groupIndicators,
+predict.SL.glasso = function(object,newdata,...){
+    predict.glasso = predict(object,
+               newdata,
+               s="lambda.min",
+               type='link')
+    pred = sapply(1:length(predict.gglasso),
+              function(x)1/(1+exp(-predict.gglasso[x])))
+    return(pred)
+    }    
+    
+    
+######### sparse group lasso penalization
+SL.sparseglasso = function(Y, X, newX, family, 
+                             #  grouping = groupIndicators,
                                alpha = 0.5,
                                standardize = FALSE,
-                               lambda= lambdaseq,
+                               lambda= seq(1,0.0001,length=10),
+                                grouping = 1:dim(X)[2],
                                ...)
     {  
         require(msgl) #package to fit sparse group lasso
+     #   grouping = getGroupsFoldsAVR(TT,X,Y)$groupIndicators
         if (family$family == "gaussian") {
             stop("SL.sparseglasso only available for family = binomial()")
         }
@@ -111,7 +165,7 @@ SLwrapperALL = function(X,Y,method,groupIndicators,
                                    lambda=lambda)
         lambda.min = lambda[best_model(fit.CVspglasso)]
         fit.msgl <- msgl::fit(x=X, classes=Y, alpha = alpha,
-                              grouping =groupIndicators,
+                              grouping =grouping,
                               lambda = lambda.min,d=1,
                               standardize = FALSE)
         predict.test.msgl = cbind(1,newX)%*%fit.msgl$beta[[1]][2,] 
@@ -122,9 +176,16 @@ SLwrapperALL = function(X,Y,method,groupIndicators,
         class(out$fit) <- 'SL.sparseglasso'
         return(out)
     }
+
+predict.SL.sparseglasso =  function(object,newdata,...){
+    predict.test.msgl = cbind(1,newX)%*%fit.msgl$beta[[1]][2,] 
+    pred = sapply(1:length(predict.test.msgl),
+                  function(x)1/(1+exp(-predict.test.msgl[x])))
+    return(pred)
+}
     
-    ####### logistic regression with firth's adjustment for sparse outcome
-    SL.logistf = function(Y, X, newX, ...)
+####### logistic regression with firth's adjustment for sparse outcome
+SL.logistf = function(Y, X, newX, ...)
     {
         require(logistf)
         if (!is.matrix(X)) {
@@ -132,7 +193,8 @@ SLwrapperALL = function(X,Y,method,groupIndicators,
             newX <- model.matrix(~ -1 + ., newX)
         }
         dtf = data.frame(Y,X)
-        fit = logistf( as.formula(paste("Y~",paste(dimnames(X)[[2]],collapse = '+'))),
+        fit = logistf(as.formula(paste("Y~",
+                        paste(dimnames(X)[[2]],collapse = '+'))),
                        data = dtf, pl = TRUE, 
                        alpha = 0.05, 
                        firth = TRUE,  
@@ -143,30 +205,21 @@ SLwrapperALL = function(X,Y,method,groupIndicators,
         out = list(pred=pred,fit=fit)
         class(out$fit) = 'SL.logistf'
         return(out)
-    }
-    
-    ##Specify a library of algorithms##
-    create.spgl = create.Learner("SL.sparseglasso",
-                                 tune = list(alpha=c(0.5)))
-    SL.library <- c("SL.glm","SL.mean",
-                    "SL.randomForest",
-                    "SL.glmnet",
-                    "SL.logistf",
-                   "SL.glasso",create.spgl$names)
-    
-    ##Run the super learner to obtain final predicted values for 
-    ##the super learner as well as CV risk for algorithms in the library##
-    fit.data.SL<- CV.SuperLearner(Y=Y,X=X,
-                                  SL.library=SL.library, 
-                                  family=binomial(),
-                                  method= method,
-                                  V=5,
-                                  cvControl = list(stratifyCV = TRUE),
-                                  verbose=TRUE)
-    return(fit.data.SL)
 }
 
+predict.SL.logistf = function(object,newdata,...){
+    if (!is.matrix(newdata)) {
+        newdata <- model.matrix(~ -1 + ., newdata)
+    }
+    pred.vals = cbind(1,newdata)%*%object$coef 
+    pred = sapply(1:length(pred.vals),
+                  function(x)1/(1+exp(-pred.vals[x])))
+    return(pred)
+}
+    
 
+##Evalution metrics
+##
 #Probability of Detection = sum true positive/sum condition positive observed
 TPRfunc = function(predictions,trueVal){
     length(which(predictions==1&trueVal==1))/length(trueVal==1)
@@ -178,7 +231,15 @@ FPRfunc = function(predictions,trueVal){
     length(which(predictions==1&trueVal==0))/length(which(trueVal==0))
 } 
 
+#positive predictive value
+# sum true positive / (sum true positive + sum falce positives)
+PPVfunc = function(predictions,trueVal){
+    length(which(predictions==1&trueVal==1))/
+        (length(which(predictions==1&trueVal==1))+
+             length(which(predictions==1&trueVal==0)))
+}
 
+##misclassification error
 MCerrfunc = function(predictions,trueVal){
     1- (length(which(predictions==1&trueVal==1))+
             length(which(predictions==0&trueVal==0)))/length(trueVal)
